df531b770397a4a94212f13b1e44b9150943ddcd
Harshal Dhake <hmd0404@gmail.com>
2022-01-13 09:57:36 +0530

    heroku-required changes

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const { MONGODB } = require('./config');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
+//Global Variables
+const PORT = process.env.PORT || 5000;
+
 //Server Code
 const server = new ApolloServer({
     typeDefs,
@@ -19,8 +22,10 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true})
     .then(() => {
         console.log("Database Connected Successfully!");
-        return server.listen({port: 5000});
+        return server.listen({port: PORT});
     })
     .then(res => {
         console.log(`Server Running at ${res.url}`);
+    }).catch(err => {
+        console.error(err);
     });
