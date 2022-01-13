e8534261584c57de2c4cfa914eac976b048053c0
Harshal Dhake <hmd0404@gmail.com>
2022-01-13 12:09:59 +0530

    added cors to Apollo Server

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req }),
+    cors: {
+        origin: '*',
+        credentials: true
+    }
 });
 
 mongoose.connect(MONGODB, { useNewUrlParser: true})
